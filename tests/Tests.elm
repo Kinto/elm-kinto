ffd1223c352ab156c1aa789f2c9c9284ea1baaa7
Nicolas Perriault <nicolas@perriault.net>
2016-09-16 08:56:16 +0200

    Concise tests.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -5,52 +5,33 @@ import Expect
 import Utils exposing (timeAgo)
 
 
-timeDiff : Float -> String
-timeDiff diff =
+timeAgoTest : String -> Float -> String -> Test
+timeAgoTest description diff result =
     let
         now =
             1473870908000
 
         current =
             now - (diff * 1000)
+
+        subject =
+            timeAgo current now
     in
-        timeAgo current now
+        test description <| \() -> Expect.equal subject result
 
 
 all : Test
 all =
     describe "Date.TimeAgo"
-        [ test "<1mn diff" <|
-            \() ->
-                Expect.equal (timeDiff 30) "less than a minute ago"
-        , test "=1mn diff" <|
-            \() ->
-                Expect.equal (timeDiff 60) "a minute ago"
-        , test "<1h diff" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 30)) "30 minutes ago"
-        , test "=1h diff" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60)) "an hour ago"
-        , test "<1d diff" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60 * 12)) "12 hours ago"
-        , test "=1d diff" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60 * 24)) "a day ago"
-        , test "<1month diff" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60 * 24 * 12)) "12 days ago"
-        , test "=1month diff" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60 * 24 * 31)) "a month ago"
-        , test "<1month diff, week match" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60 * 24 * 14)) "2 weeks ago"
-        , test "<1year diff" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60 * 24 * 30 * 6)) "6 months ago"
-        , test ">1 year" <|
-            \() ->
-                Expect.equal (timeDiff (60 * 60 * 24 * 365 * 12)) "12 years ago"
+        [ timeAgoTest "<1mn diff" 30 "less than a minute ago"
+        , timeAgoTest "=1mn diff" 60 "a minute ago"
+        , timeAgoTest "<1h diff" (60 * 30) "30 minutes ago"
+        , timeAgoTest "=1h diff" (60 * 60) "an hour ago"
+        , timeAgoTest "<1d diff" (60 * 60 * 12) "12 hours ago"
+        , timeAgoTest "=1d diff" (60 * 60 * 24) "a day ago"
+        , timeAgoTest "<1month diff" (60 * 60 * 24 * 12) "12 days ago"
+        , timeAgoTest "=1month diff" (60 * 60 * 24 * 31) "a month ago"
+        , timeAgoTest "<1month diff, week match" (60 * 60 * 24 * 14) "2 weeks ago"
+        , timeAgoTest "<1year diff" (60 * 60 * 24 * 30 * 6) "6 months ago"
+        , timeAgoTest ">1 year" (60 * 60 * 24 * 365 * 12) "12 years ago"
         ]
