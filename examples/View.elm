b02458831f5f5588ee0694e24fc2e602b0331458
Mathieu Agopian <mathieu@agopian.info>
2016-12-03 09:48:22 +0100

    Address review comments from @n1k0

--- examples/View.elm
+++ examples/View.elm
@@ -39,6 +39,20 @@ recordRow currentTime { id, title, description, last_modified } =
         ]
 
 
+headingWithSortIcon : String -> String -> String -> Html.Html Msg
+headingWithSortIcon icon sortColumn heading =
+    let
+        content =
+            if heading == sortColumn then
+                [ Html.i [ Html.Attributes.class icon ] []
+                , Html.text <| " " ++ heading
+                ]
+            else
+                [ Html.text heading ]
+    in
+        Html.th [ Html.Events.onClick (SortByColumn heading) ] content
+
+
 recordHeaders : Sort -> List (Html.Html Msg)
 recordHeaders sort =
     let
@@ -52,19 +66,6 @@ recordHeaders sort =
 
         headings =
             [ "id", "title", "description", "last_modified" ]
-
-        headingWithSortIcon : String -> String -> String -> Html.Html Msg
-        headingWithSortIcon icon sortColumn heading =
-            let
-                content =
-                    if heading == sortColumn then
-                        [ Html.i [ Html.Attributes.class icon ] []
-                        , Html.text <| " " ++ heading
-                        ]
-                    else
-                        [ Html.text heading ]
-            in
-                Html.th [ Html.Events.onClick (SortByColumn heading) ] content
     in
         List.map
             (headingWithSortIcon icon sortColumn)
@@ -96,7 +97,7 @@ errorNotif error =
 view : Model -> Html.Html Msg
 view { error, records, formData, currentTime, sort } =
     Html.div [ Html.Attributes.class "container" ]
-        [ Html.h1 [] [ Html.text "Kinto Elm :-)" ]
+        [ Html.h1 [] [ Html.text "elm-kinto demo" ]
         , Html.small
             []
             [ Html.text "Will only display 30 records at most" ]
